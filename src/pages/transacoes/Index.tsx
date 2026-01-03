ac732591f644351422296714416da02b5a102f31
Paulo André <pauloandrepaulinho05@gmail.com>
2026-01-03 10:44:42 +0000

    feat: Transacoes

--- src/pages/transacoes/Index.tsx
+++ src/pages/transacoes/Index.tsx
@@ -1,3 +1,13 @@
+import { HeaderTransacoes } from "../../components/transacoes/header/Index"
+
 export const Transacoes = () => {
-    return <h1 className="text-2xl font-bold">Transações</h1>
+    return (
+        <div className="flex flex-col gap-4">
+            <div className="flex justify-between items-center">
+                <h1 className="text-2xl font-semibold">Transações</h1>
+                <button className="px-4 py-2 bg-secondary text-white font-semibold dark:bg-primary rounded-md">+Nova</button>
+            </div>
+            <HeaderTransacoes />
+        </div>
+    )
 }
