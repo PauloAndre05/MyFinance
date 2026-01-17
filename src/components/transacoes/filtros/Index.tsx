281112bc22478762107436ecbad9dc41333e8367
Paulo André <pauloandrepaulinho05@gmail.com>
2026-01-17 21:46:32 +0000

    feat: filtros e modais na página de transações

--- src/components/transacoes/filtros/Index.tsx
+++ src/components/transacoes/filtros/Index.tsx
@@ -1,36 +1,53 @@
-import { dadosTransacoes } from "../dadosTransacoes";
+import { format } from "date-fns"
 
 interface FiltrosTransacoesProps {
-  filteredTransacoes: typeof dadosTransacoes;
-} 
+  onSearch: (term: string) => void
+}
 
-export const FiltrosTransacoes = ({ filteredTransacoes }: FiltrosTransacoesProps) => {
-  /* Pesquisa de transações */
-  function handleSearchTransacoes(value: string) {
-    const resultados = value? filteredTransacoes.filter((transacao) => {
-      return transacao.descricao.toLowerCase().includes(value.toLowerCase()) || transacao.categoria.toLowerCase().includes(value.toLowerCase());
-    })
-    : dadosTransacoes;
-    filteredTransacoes = resultados;
-  }
+export const FiltrosTransacoes = ({ onSearch }: FiltrosTransacoesProps) => {
+  const date = new Date();
+  const allmonths = Array.from({ length: 12 }, (_, i) => {
+    const monthDate = new Date(date.getFullYear(), i, 1);
+    return monthDate;
+  })
   return (
-    <div className="flex flex-wrap gap-3">
-      <select className="border-b outline-0 rounded-md w-40 border-primary cursor-pointer">
+    <div className="flex flex-wrap gap-5">
+
+      <select className="border-b outline-0 rounded-md w-40 border-primary cursor-pointer text-sm" onChange={(e) => onSearch(e.target.value)}>
         <option value="" className="text-text-primary cursor-pointer w-full">
           Mês
         </option>
+        {allmonths.map((month, index) => (
+          <option value={format(month, "MM")} className="text-text-primary cursor-pointer w-full" key={index}>
+            {format(new Date(month), "MMMM")}
+          </option>
+        ))}
       </select>
-      <select name="" id="" className="border-b outline-0 rounded-md w-40 border-primary cursor-pointer">
-        <option value="" className="text-text-primary cursor-pointer w-full">
-          Categotia
+
+      <select name="" id="" className="border-b outline-0 rounded-md w-40 border-primary cursor-pointer text-sm" onChange={(e) => onSearch(e.target.value)}>
+        <option value="" className="text-text-primary cursor-pointer w-full" >
+          Selecione a Categoria
+        </option>
+        <option value="Alimentação" className="text-text-primary cursor-pointer w-full">
+          Alimentação
+        </option>
+        <option value="Transporte" className="text-text-primary cursor-pointer w-full">
+          Transporte
         </option>
       </select>
-      <select name="" id="" className="border-b outline-0 rounded-md w-40 border-primary cursor-pointer">
+
+      <select name="" id="" className="border-b outline-0 rounded-md w-40 border-primary cursor-pointer text-sm" onChange={(e) => onSearch(e.target.value)}>
         <option value="" className="text-text-primary cursor-pointer w-full">
-          Tipo
+          Tipo de Transação
+        </option>
+        <option value="Receita" className="text-text-primary cursor-pointer w-full">
+          Receita
+        </option>
+        <option value="Despesa" className="text-text-primary cursor-pointer w-full">
+          Despesa
         </option>
       </select>
-      <input className="border-b p-2 outline-0 rounded-md w-40 border-primary" placeholder="Pesquisar..." onChange={ (e) => handleSearchTransacoes(e.target.value)}/>
+      <input className="border-b p-2 outline-0 rounded-md w-40 border-primary" placeholder="Pesquisar..." onChange={(e) => onSearch(e.target.value)} />
     </div>
   );
 };
--- src/components/transacoes/headerTransacoes/Index.tsx
+++ src/components/transacoes/headerTransacoes/Index.tsx
@@ -1,8 +1,12 @@
 import { format } from "date-fns";
+import { dadosTransacoes } from "../dadosTransacoes";
+
 interface HeaderTransacoesProps {
   isOpenModal?: boolean;
   setIsOpenModal?: (isOpen: boolean) => void;
 }
+
+const totalTransacoes = dadosTransacoes.length;
 
 export const HeaderTransacoes = ({
   isOpenModal,
@@ -19,17 +23,21 @@ export const HeaderTransacoes = ({
   }
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex flex-col gap-1">
-        <h1 className="text-2xl font-semibold">Transações</h1>
-        <p className="text-sm">{`${currentMonth} de ${currenteYear} . 18 transações`}</p>
+    <div className="flex flex-col">
+      <div className="flex justify-between items-center ">
+        <div className="flex flex-col gap-1">
+          <h1 className="text-2xl font-semibold">Transações</h1>
+          <p className="text-sm">{`${currentMonth} de ${currenteYear} . ${totalTransacoes} transações`}</p>
+        </div>
+        <button
+          className="px-4 py-2 text-white font-semibold bg-primary rounded-md cursor-pointer"
+          onClick={handleOpenModal}
+        >
+          Nova transação
+        </button>
       </div>
-      <button
-        className="px-4 py-2 text-white font-semibold bg-primary rounded-md cursor-pointer"
-        onClick={handleOpenModal}
-      >
-        Nova transação
-      </button>
+      <div className="mt-5 border-b-4 border-orange-400 w-11" />
+
     </div>
   );
 };
