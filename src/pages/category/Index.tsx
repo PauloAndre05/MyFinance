7da411c08d5b6180e8dcf4f6829562b03a5e131b
Paulo André <pauloandrepaulinho05@gmail.com>
2026-02-13 23:18:17 +0000

    fix: localStorage

--- src/pages/category/Index.tsx
+++ src/pages/category/Index.tsx
@@ -6,26 +6,40 @@ import { CategoryService } from "../../services/CategoryServices";
 
 export const Category = () => {
   const [categories, setCategories] = useState<CategoryInterface[]>([]);
+  const [view, setView] = useState<"grid" | "list">(() => {
+    const saved = localStorage.getItem("@finance:view2026");
+    return saved ? JSON.parse(saved) : "grid";
+  });
 
   useEffect(() => {
+
     async function loadCategories() {
       const data = await CategoryService.getAll();
       setCategories(data);
     }
+
     loadCategories();
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("@finance:view2026", JSON.stringify(view));
+  }, [view]);
 
   /* Deletar Categoria */
 
   async function DeleteCategory(id: string) {
-    CategoryService.remove(id);
+    await CategoryService.remove(id);
     setCategories((prev) => prev.filter((category) => category.id !== id));
   }
 
   return (
     <div className="flex flex-col gap-4">
-      <HeaderCategory />
-      <CategoryList categories={categories} onDelete={DeleteCategory}/>
+      <HeaderCategory handleToggleView={setView} />
+      <CategoryList
+        categories={categories}
+        onDelete={DeleteCategory}
+        view={view}
+      />
     </div>
   );
 };
